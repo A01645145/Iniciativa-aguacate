2a7798ee6058497a248dde7e230ac61b677ba6cf
Adriana G. <141658166+A01645145@users.noreply.github.com>
2026-03-12 11:07:05 -0600

    Update proyecto_sensor_humedad.ino

--- proyecto_sensor_humedad.ino
+++ proyecto_sensor_humedad.ino
@@ -1,46 +1,54 @@
-// Incluimos librería
+//Librerías
 #include <DHT.h>
 #include "LiquidCrystal.h"
 
-
+//Definimos el indicador
 #define LED_ROJO 10
 
 // Definimos el pin digital donde se conecta el sensor
-#define DHTPIN 2
-// Dependiendo del tipo de sensor
-#define DHTTYPE DHT11
+#define DHTPIN 2 
 
+// Dependiendo del tipo de sensor
+#define DHTTYPE DHT11 
+
+//Definimos la humedad mínima requerida
+int humedad_requerida = 36;
 
 // Inicializamos el sensor DHT11
 DHT dht(DHTPIN, DHTTYPE);
- 
+
+//Se definen las constantes para la LCD
 int rs = 8;
 int enable = 9;
 int d4 = 4;
 int d5 = 5;
 int d6 = 6;
 int d7 = 7;
-int humedad_requerida = 36;
-
 LiquidCrystal lcd(rs, enable, d4, d5, d6, d7);
 
 
 void setup() {
   // Inicializamos comunicación serie
   Serial.begin(9600);
-  pinMode(BUZZER_PIN, OUTPUT);
+  // Inicializamos LCD
   lcd.begin(16, 2);
-  pinMode(LED_ROJO,OUTPUT);
   // Comenzamos el sensor DHT
   dht.begin();
+  //Símbolo de grados
   byte circulo[] = { B00111, B00101, B00111, B00000, B00000, B00000, B00000, B00000 };  
   lcd.createChar(0, circulo);
+ //Indicador
+  pinMode(LED_ROJO,OUTPUT);
+
 }
  
 void loop() {
-    // Esperamos 5 segundos entre medidas
+   // Esperamos 1 segundo entre medidas
   delay(1000);
-  digitalWrite(LED_ROJO, LOW);
+
+  //Se apaga el indicador al iniciar cada vuelta
+  digitalWrite(LED_ROJO, LOW); 
+ 
   // Leemos la humedad relativa
   float h = dht.readHumidity();
   // Leemos la temperatura en grados centígrados (por defecto)
@@ -58,17 +66,22 @@ void loop() {
   float hif = dht.computeHeatIndex(f, h);
   // Calcular el índice de calor en grados centígrados
   float hic = dht.computeHeatIndex(t, h, false);
- 
+
+  //Iniciamos en la primera casilla
   lcd.setCursor(0, 0);
+  //Se imprime la humedad
   lcd.write("Humedad: ");
   lcd.print(h);
+  //Iniciamos en el segundo renglón
   lcd.setCursor(0, 1);
+  //Se imprime la temperatura
   lcd.write("Temp: ");
   lcd.print(t);
   lcd.write(byte(0));
   lcd.write("C ");
 
   if (h<humedad_requerida) {
+      //Si la humedad es menor a la requerida, se enciende el LED
      digitalWrite(LED_ROJO, HIGH);
   }
 
